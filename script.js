93d1f2ba4646198dff224ae2440268930fc3d62f
Jarosław Lewczuk <jar.lewczuk@gmail.com>
2019-10-29 23:14:00 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) +90;
+
+    const mins = now.getMinutes();
+    const minutesDegrees = ((mins / 60) * 360) +90;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
